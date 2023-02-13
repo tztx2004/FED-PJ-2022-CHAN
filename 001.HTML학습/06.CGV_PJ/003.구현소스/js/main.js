5a39d29fbea255ff5b38a67aadf02a9ff0d48f75
tztx2004 <tztx2004@gmail.com>
2023-02-13 16:28:10 +0900

    2302013 오후

--- 001.HTML학습/06.CGV_PJ/003.구현소스/js/main.js
+++ 001.HTML학습/06.CGV_PJ/003.구현소스/js/main.js
@@ -1,11 +1,11 @@
 // CGV PJ 메인 페이지 JS - main.js
 
 // 요소선택함수 ////////
-const qs = x=>document.querySelector(x);
-const qsa = x=>document.querySelectorAll(x);
+const qs = (x) => document.querySelector(x);
+const qsa = (x) => document.querySelectorAll(x);
 
 //////////////////로드 구역/////////////////////
-window.addEventListener("DOMContentLoaded",()=>{
+window.addEventListener("DOMContentLoaded", () => {
     console.log("로딩완료");
 
     /*************************************** 
@@ -20,20 +20,20 @@ window.addEventListener("DOMContentLoaded",()=>{
 
     // 2. 클릭이벤트 함수 설정하기
     // for of 사용 -> 배열/컬렉션 일때 사용!
-    for(let x of mlist){// x는 각 li요소
-        
-        x.onclick = ()=>{
+    for (let x of mlist) {
+        // x는 각 li요소
+
+        x.onclick = () => {
             // 1. li에 클래스 초기화
-            mlist.forEach(ele=>ele.classList.remove("on"));
+            mlist.forEach((ele) => ele.classList.remove("on"));
             // forEach((요소,순번,객체)=>{코드})
-        
+
             // 2. 클릭된 li에 클래스"on"넣기
             x.classList.add("on");
             // classList 객체 사용
             // add() 메서드 사용
         }; ///////// onclick //////////
-
-    }///////////////for of//////////////
+    } ///////////////for of//////////////
 
     /********************************************* 
         [ 극장가는 길 구글맵보기 기능 ]
@@ -41,14 +41,62 @@ window.addEventListener("DOMContentLoaded",()=>{
         - 구글맵박스의 닫기 버튼 클릭 시 구글맵 퇴장
         -> 방법: CSS셋팅된 클래스 "on"넣기/빼기
     *********************************************/
-    const anibx = qs(".anibx")
-    for(x of anibx){
-        x.onclick = ()=>{};
-    }
+    // 1. 대상선정
+    // 1-1.이벤트 대상 : .anibx -> 애니메이션 버튼박스
+    const anibx = qs(".anibx");
+    // 1-2.이벤트 대상 : .cbtn -> 닫기버튼
+    const cbtn = qs(".cbtn");
 
+    // 1-3.변경대상 : .gmap -> 구글맵박스
+    const gmap = qs(".gmap");
 
+    // 2. 버튼에 클릭이벤트 설정하기
+    // -> 구글맵박스에 클래스 넣기
+    anibx.onclick = () => {
+        gmap.classList.add("on");
+        anibx.classList.add("on");
+    };
 
+    // 3.닫기버튼에 클릭이벤트 설정하기
+    // -> 클릭 시 구글맵박스에 클래스 빼기
+    cbtn.onclick = () => {
+        gmap.classList.remove("on");
+        anibx.classList.remove("on");
+    };
 
-    
+    /********************************************* 
+        [ SNS버튼 링크 이동 ]
+    *********************************************/
+    // 1. 대상선정 : .sns a
+    const sns = qsa(".sns a");
+    // 2. 클릭 이벤트 설정
+    // -> forEach((요소,순번,객체)=>{코드})
+    sns.forEach((ele) => {
+        ele.onclick = () => {
+            // 1. 내부텍스트
+            let btxt = ele.innerText;
+            // console.log(btxt);
 
-});///////////로드 구역/////////////////////////
+            // 이동주소 변수
+            let url = "";
+
+            // 2. 분기문 utl할당
+            switch (btxt) {
+                case "페이스북 바로가기":
+                    url = "https://www.facebook.com/CJCGV";
+                    break;
+                case "트위터 바로가기":
+                    url = "https://twitter.com/CGV_ID";
+                    break;
+                case "인스타그램 바로가기":
+                    url = "https://www.instagram.com/cgv_korea/";
+                    break;
+            } //////// switch case문 ////////////
+
+            // 3. 페이지 이동하기 : 새창으로!
+            // 새창 -> window.open()
+            // 페이지이동 -> location.href = url값
+            window.open().location.href = url;
+        }; //////////click/////////////////////
+    }); /////////////// forEach /////////////////
+}); ///////////로드 구역/////////////////////////
