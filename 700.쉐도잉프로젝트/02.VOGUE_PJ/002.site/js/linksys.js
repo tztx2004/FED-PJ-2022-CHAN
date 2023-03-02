710fa4e673fd26aaf0385345ac8e02d786e49c49
tztx2004 <tztx2004@gmail.com>
2023-03-02 13:19:54 +0900

    230302 오전

--- 700.쉐도잉프로젝트/02.VOGUE_PJ/002.site/js/linksys.js
+++ 700.쉐도잉프로젝트/02.VOGUE_PJ/002.site/js/linksys.js
@@ -1,12 +1,48 @@
 // 보그 PJ linksys JS - linksys.js
 
+
 //////////// 로딩구역 //////////////
 window.addEventListener("DOMContentLoaded",linkFn);
 
 ////////////////////// 링크시스템 로드함수 ///////////////////////
 function linkFn(){
     
-    console.log("로딩완료");
-    
+    console.log("링크 로딩완료");
 
+    // 1. 링크 대상 선정 : 
+    // (1) GNB : .gnb a
+    // (2) logo : .logo a
+    const gnb = document.querySelectorAll(".gnb a");
+    const logo = document.querySelector(".logo a");
+    console.log(logo);
+
+    // 2. 클릭 이벤트 설정하기
+    // (1) GNB 클릭설정////////////
+    for(let x of gnb){
+        x.onclick = (e)=>{
+            // 클릭이동기능막기
+            event.preventDefault();
+
+            // (1) 클릭된 a요소 텍스트 읽기
+            const atxt = x.innerText.toLowerCase().trim();
+            // toLowerCase() -> 소문자변환
+            // 참고) toUpperCase() -> 대문자변환
+            // trim() -> 앞뒤공백제거
+            console.log(atxt);
+
+            // (2) 서브페이지 이동하기
+            if(atxt !== search) // search가 아니면 서브이동!
+            location.href = "category.html?cat=" + encodeURIComponent(atxt);
+            
+
+        };///////////////// onclick //////////////////
+    } //////////////////// for of //////////////////////
+
+    // (2) 로고 클릭설정
+    logo.onclick = (e)=>{
+        event.preventDefault();
+
+        // 홈으로 이동하기
+        location.href = "index.html";
+    }; ///////////////// onclick //////////////////
 }////////////////////// linkFn 함수 //////////////////////////
