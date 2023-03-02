6728a83325d9635cddeb91a865b06d7f53db7973
tztx2004 <tztx2004@gmail.com>
2023-03-02 12:28:35 +0900

    230302 오전

---
+++ 700.쉐도잉프로젝트/02.VOGUE_PJ/002.site/js/categoty.js
@@ -0,0 +1,12 @@
+// 보그 PJ 카테고리 페이지 JS - category.js
+
+//////////// 로딩구역 //////////////
+window.addEventListener("DOMContentLoaded",loadFn);
+
+//////////// 로드함수 ///////////////
+function loadFn(){
+    
+    console.log("로딩완료");
+    
+
+}//////////// loadFn 함수 /////////////////
---
+++ 700.쉐도잉프로젝트/02.VOGUE_PJ/002.site/js/linksys.js
@@ -0,0 +1,12 @@
+// 보그 PJ linksys JS - linksys.js
+
+//////////// 로딩구역 //////////////
+window.addEventListener("DOMContentLoaded",linkFn);
+
+////////////////////// 링크시스템 로드함수 ///////////////////////
+function linkFn(){
+    
+    console.log("로딩완료");
+    
+
+}////////////////////// linkFn 함수 //////////////////////////
