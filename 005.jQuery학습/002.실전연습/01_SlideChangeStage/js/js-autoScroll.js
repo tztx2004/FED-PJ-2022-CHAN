7b06388f69e84f72599bc658c1bb50d3929a66f6
tztx2004 <tztx2004@gmail.com>
2023-03-07 12:03:37 +0900

    230307 오전

--- 005.jQuery학습/002.실전연습/01_SlideChangeStage/js/js-autoScroll.js
+++ 005.jQuery학습/002.실전연습/01_SlideChangeStage/js/js-autoScroll.js
@@ -153,10 +153,8 @@ function loadFn(){
         } ////// else //////
         // console.log("페이지번호",pgnum);
 
-        // (4) 페이지이동
-        // scrollTo(가로,세로)
-        window.scrollTo(0,window.innerHeight * pgnum);
-        // 세로 이동위치 : 윈도우높이값 * 페이지번호
+        // (4) 페이지이동 + 메뉴변경 -> updatedPg 함수호출
+        updatePg(gnb);
 
     } ////////////////// wheelFn /////////////////////
 
@@ -172,14 +170,31 @@ function loadFn(){
         // 3. 페이지번호(pgnum) 업데이트하기!
         pgnum = seq;
         console.log("메뉴클릭 페이지번호 : ",pgnum)
-        // 4. 페이지 이동하기
-        window.scrollTo(0,window.innerHeight * pgnum);
-        // 5. 메뉴초기화하기(class on 제거하기)
-        for(let x of obj) x.parentElement.classList.remove("on");
-        // 6. 해당메뉴에 클래스 넣기
-        obj[seq].parentElement.classList.add("on");
+        // 4. 업데이트 페이지 호출 -> 페이지이동,메뉴변경
+        updatePg(obj);
 
     }/////////////// movePg //////////////////
+
+    /****************************************** 
+        함수명 : updatePg
+        기능 : 페이지 이동 시 설정값 업데이트하기
+    ******************************************/
+    function updatePg(obj){ // obj - 변경할 메뉴전체 객체
+        // 1. 함수호출확인
+        console.log("업데이트");
+        
+        // 2. 페이지 이동하기
+        // scrollTo(가로,세로)
+        window.scrollTo(0,window.innerHeight * pgnum);
+        // 세로 이동위치 : 윈도우높이값 * 페이지번호
+        
+        // 3. 메뉴초기화하기(class on 제거하기)
+        for(let x of obj) x.parentElement.classList.remove("on");
+        
+        // 4. 해당메뉴에 클래스 넣기
+        obj[pgnum].parentElement.classList.add("on");
+    }///////////// updatePg ////////////////
+
 
 
 };/////////////////////////////// loadFn /////////////////////////
