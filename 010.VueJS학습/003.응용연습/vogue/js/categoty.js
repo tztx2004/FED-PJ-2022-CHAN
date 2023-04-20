fc3275f12b28f872995a59b92c72f90c569a4f06
tztx2004 <tztx2004@gmail.com>
2023-04-20 16:21:18 +0900

    230420 오후

--- 010.VueJS학습/003.응용연습/vogue/js/categoty.js
+++ 010.VueJS학습/003.응용연습/vogue/js/categoty.js
@@ -24,7 +24,7 @@ $(()=>{
 
 
     // vue.js 데이터 바인딩 코드 //
-    new Vue({
+    const vmCont = new Vue({
         // 대상선정: 메인 컨텐츠영역요소
         el:"#cont",
         data:{
@@ -38,8 +38,44 @@ $(()=>{
             // 엑시오스로 제이슨연결하여 데이터 가져오기
             axios.get("./js/mdata.json")
             .then(x=>this.items=x);
+
+            
         }
-    })
+    });///////////////// Vue /////////////////////
+
+    // GNB 메뉴 SPA를 위한 재정의하기!
+    // -> VueJS 인스턴스의 생성하면 대상요소가
+    // 기존 JS의 모든 기능이 초기화하여 작동되지 않는다!
+    new Vue({
+        // 대상선정 : #gnb
+        el:"#gnb",
+        data:{
+
+        },
+        methods:{
+            // 메뉴 변경하기
+            chgMenu(){
+                // 컨텐츠영역의 뷰에 설정된 변수 catName에
+                // 접근하여 변수의 값을 변경한다!
+                // vmCont변수에 인스턴스가 담겼으므로
+                // vmCont.catName으로 접근하여
+                // 클릭된 요소의 글자를 읽고 이를 소문자로 변경
+                // " & "를 "-"로 치환만 해주면 된다!
+                
+                // a요소 문자열 변경하기
+                let txt = 
+                event.target.innerText.replace(" & ","-").toLowerCase();
+                console.log(txt)
+                
+                // 뷰데이터에 반영하기
+                vmCont.catName = txt;
+
+                // 탭메뉴 타이틀 변경하기
+                $("title").text(vmCont.catName + " | 2023보그 코리아(Vogue Korea)");
+            }
+        },/////// methods 구역 ///////
+
+    }); /////////// GNB Vue ///////////////
 
 
 
