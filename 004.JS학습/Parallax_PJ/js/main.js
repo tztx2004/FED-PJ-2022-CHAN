2fbe85f43375426479aba5d6ef1f91424228ed41
tztx2004 <tztx2004@gmail.com>
2023-04-21 11:10:47 +0900

    230421 오전

--- 004.JS학습/Parallax_PJ/js/main.js
+++ 004.JS학습/Parallax_PJ/js/main.js
@@ -8,7 +8,7 @@ function loadFn(){
     console.log("로딩완료");
 
     // 1. 부드러운 스크롤 적용하기
-    startSS();
+    startSS(); 
 
     // 2. 공통 선택자함수
     const qs = x => document.querySelector(x);
@@ -30,13 +30,60 @@ function loadFn(){
     const tg1 = qsa(".txt");
     // (2). 아이콘
     const tg2 = qsa(".icon");
+
+    // 7. 페럴렉스 이동함수 //
+    const moveEl = (elpos,ele,setH)=>{
+        // 전달값: elpos - 위치값 / ele  -요소 / setH - 정한범위
+
+        // 패럴렉스 범위 : 윈도우 높이값 ~0
+        // 화면에서 완전히 사라질때 범위는 0이 아니고
+        // 요소의 -높이값 만큼 이다! -> 적당히 -수치로 해결!
+        if(elpos < winH && elpos > 0){
+
+        // 1. 위치이동값 계산
+        let x = setH - (elpos *setH) / winH;
+        console.log(-x);
+
+        // 2. 해당요소에 위치이동 CSS반영
+        ele.style.cssText = `
+            transform:translateY(${-x}px)
+        `;
+        // cssText 속성은 style속성값을 직접넣어준다!
+
+        // 대상요소의 트랜스폼 Y축이동
+        // 위치이동의 계산원리
+        // -> 윈도우화면 : 위치값 = 정한범위 : 실제이동값
+        // -> 윈도우가 1000px : 500px = 200px : x?
+        // 실제이동값 = 정한범위 - (위치값*정한범위) / 윈도우화면
+        // x = setH2 - (elpos *setH2) / winH
+        // -> 이동수치는 0부터 서서히 증가해야하므로
+        // 정한범위수에서 빼준다!
+        }//////////////// 페럴렉스 범위 if ///////////////////
+    };///////////// moveEl ////////////
     
-    // 7. 스크롤 이벤트함수 만들기
+    // 8. 스크롤 이벤트함수 만들기
     window.addEventListener("scroll",()=>{
         
-        // 테스트 - 3번째 텍스트 위치값
-        console.log(retVal(tg1[2]));
+        // 1. 대상1 : 글자박스 페럴렉스호출!
+        tg1.forEach(ele=>{
+            moveEl(retVal(ele),ele,setH2);
 
+            // moveEl(위치값,요소,정한범위)
+        });
+        // 2. 대상2 : 아이콘 페럴렉스호출!
+        tg2.forEach(ele=>{
+            moveEl(retVal(ele),ele,setH1);
+        });
+        
     });////////////////// scroll /////////////////////
+
+    // 로딩시 맨 위로 이동
+    setTimeout(()=>{
+        // 맨위로 이동
+        window.scrollTo(0,0);
+        // 부드러운 스크롤 위치값 반영!
+        pos = 0;
+    },400)
+
 
 }///////////////////// loadFn ////////////////////////
