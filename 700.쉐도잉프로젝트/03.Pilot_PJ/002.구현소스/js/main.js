a65110aa421b71e8262e3d7af2cec2ca58401ce3
tztx2004 <tztx2004@gmail.com>
2023-04-24 16:41:46 +0900

    230424 오전

--- 700.쉐도잉프로젝트/03.Pilot_PJ/002.구현소스/js/main.js
+++ 700.쉐도잉프로젝트/03.Pilot_PJ/002.구현소스/js/main.js
@@ -288,10 +288,11 @@ const clearAuto = ()=> {
     clearInterval(banAuto);
     clearTimeout(banAgain);
     banAgain = setTimeout(banAutoSlide,5000)
+    console.log("클리어")
 }; ///////////////// clearAuto ///////////////
 
 // 배너이동 시 자동넘김 지우기 셋팅
-slide.on("mousemove dragstart dragstop",clearAuto);
+slide.on("drag dragstart dragstop",clearAuto);
 
 // 자동넘김 인터발 셋팅하기 //
 // 변수에 담아 정지하기
