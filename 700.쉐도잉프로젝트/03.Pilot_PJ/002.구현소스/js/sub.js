b9d49fdbab7411346a454e0a18a4bd20e829caa1
tztx2004 <tztx2004@gmail.com>
2023-05-02 10:34:34 +0900

    230502 오전

---
+++ 700.쉐도잉프로젝트/03.Pilot_PJ/002.구현소스/js/sub.js
@@ -0,0 +1,47 @@
+// 카테고리 서브페이지 JS - sub.js
+
+// 자동스크롤 기능 함수 가져오기
+import autoScroll from "./jquery-autoScroll.js";
+// 메뉴기능함수 가져오기
+import menuFn from "./mainjs/menu.js";
+// 배너기능함수 가져오기
+import banFn from "./mainjs/ban.js";
+// 공통데이터 가져오기
+import comData from "./tempData/data-common.js";
+
+// ####### 상단영역 메뉴 뷰템플릿 셋팅 ########//
+// Vue.component(내가지은요소명,{옵션})
+Vue.component("top-comp",{
+    template: comData.tareaSub
+}); ////////////////// 상단영역 Vue component /////////////////
+Vue.component("foot-comp",{
+    template: comData.barea
+}); ////////////////// 하단영역 Vue component /////////////////
+
+
+// ####### 상단영역 뷰 인스턴스 생성하기 #######
+// new Vue({옵션})
+new Vue({
+    el: "#top",
+    data:{},
+    // created 실행구역 : DOM연결 전
+    created:function(){
+        // DOM연결 전 데이터 가공작업
+        console.log("created구역")
+    },
+    // mounted 실행구역: DOM연결후 
+    mounted:function(){
+        // 제이쿼리코드함수 호출!
+        console.log("mounted구역")
+
+        // 메뉴기능 호출
+        menuFn();
+        
+    }
+}); ///////////// 상단영역 뷰 인스턴스 //////////////////
+
+// ####### 하단영역 뷰 인스턴스 생성하기 #######
+new Vue({
+    el:"#info",
+    data:{},
+}); /////////////// 하단영역 뷰 인스턴스 //////////////////
