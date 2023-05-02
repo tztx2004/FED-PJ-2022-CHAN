198b17c023ff3f4fbfe8f9729cfe66044ed16f19
tztx2004 <tztx2004@gmail.com>
2023-05-02 12:19:40 +0900

    230502 오전

--- 700.쉐도잉프로젝트/03.Pilot_PJ/002.구현소스/js/sub.js
+++ 700.쉐도잉프로젝트/03.Pilot_PJ/002.구현소스/js/sub.js
@@ -1,47 +1,71 @@
 // 카테고리 서브페이지 JS - sub.js
 
-// 자동스크롤 기능 함수 가져오기
-import autoScroll from "./jquery-autoScroll.js";
 // 메뉴기능함수 가져오기
 import menuFn from "./mainjs/menu.js";
-// 배너기능함수 가져오기
-import banFn from "./mainjs/ban.js";
 // 공통데이터 가져오기
 import comData from "./tempData/data-common.js";
 
+
 // ####### 상단영역 메뉴 뷰템플릿 셋팅 ########//
 // Vue.component(내가지은요소명,{옵션})
-Vue.component("top-comp",{
-    template: comData.tareaSub
+Vue.component("top-comp", {
+    template: comData.tareaSub,
 }); ////////////////// 상단영역 Vue component /////////////////
-Vue.component("foot-comp",{
-    template: comData.barea
+Vue.component("foot-comp", {
+    template: comData.barea,
 }); ////////////////// 하단영역 Vue component /////////////////
-
 
 // ####### 상단영역 뷰 인스턴스 생성하기 #######
 // new Vue({옵션})
 new Vue({
     el: "#top",
-    data:{},
+    data: {},
     // created 실행구역 : DOM연결 전
-    created:function(){
+    created: function () {
         // DOM연결 전 데이터 가공작업
-        console.log("created구역")
+        console.log("created구역");
     },
-    // mounted 실행구역: DOM연결후 
-    mounted:function(){
+    // mounted 실행구역: DOM연결후
+    mounted: function () {
         // 제이쿼리코드함수 호출!
-        console.log("mounted구역")
+        console.log("mounted구역");
 
         // 메뉴기능 호출
         menuFn();
-        
-    }
+
+        // 스와이퍼 생성함수 호출
+        makeSwiper();
+
+        // 부드러운 스크롤 실행
+        startSS();
+    },
 }); ///////////// 상단영역 뷰 인스턴스 //////////////////
 
 // ####### 하단영역 뷰 인스턴스 생성하기 #######
 new Vue({
-    el:"#info",
-    data:{},
+    el: "#info",
+    data: {},
 }); /////////////// 하단영역 뷰 인스턴스 //////////////////
+
+// 스와이퍼 플러그인 인스턴스 생성하기
+// 스와이퍼 생성함수
+function makeSwiper() {
+    var swiper = new Swiper(".mySwiper", {
+        slidesPerView: 1,
+        spaceBetween: 0,
+        loop: true,
+        autoplay: {
+            delay: 3000,
+            disableOnInteraction: false, // 인터렉션 비활성화 false 
+            // -> 인터렉션 활성화!(가만히 두면 다시 자동넘김)
+        },
+        pagination: {
+            el: ".swiper-pagination",
+            clickable: true,// 블릿클릭이동여부
+        },
+        navigation: {
+            nextEl: ".swiper-button-next",
+            prevEl: ".swiper-button-prev",
+        },
+    });
+} ////////////////////// makeSwiper ///////////////
