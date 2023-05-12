15e59da48ec79df18f1efda507b72255fc218e8e
tztx2004 <tztx2004@gmail.com>
2023-05-12 17:20:57 +0900

    updated

--- 700.쉐도잉프로젝트/03.Pilot_PJ/002.구현소스/js/sub.js
+++ 700.쉐도잉프로젝트/03.Pilot_PJ/002.구현소스/js/sub.js
@@ -18,8 +18,14 @@ let swiper;
 
 // ####### 서브영역 메뉴 뷰템플릿 셋팅 ########//
 // Vue.component(내가지은요소명,{옵션})
+// 1. 배너파트 컴포넌트
 Vue.component("ban-comp", {
     template: subData.banner,
+}); ////////////////// 서브영역 Vue component /////////////////
+
+// 2. 컨텐츠1 영역 컴포넌트
+Vue.component("cont1-comp", {
+    template: subData.cont1,
 }); ////////////////// 서브영역 Vue component /////////////////
 
 // ####### 서브영역 뷰 인스턴스 셋팅 ########//
--- 700.쉐도잉프로젝트/03.Pilot_PJ/002.구현소스/js/tempData/data-sub.js
+++ 700.쉐도잉프로젝트/03.Pilot_PJ/002.구현소스/js/tempData/data-sub.js
@@ -2,7 +2,7 @@
 
 const subData = {
     // 배너 영역
-    banner:`
+    banner: `
     <section id="ban" class="page">
         <!-- Swiper -->
         <div class="swiper mySwiper">
@@ -25,13 +25,72 @@ const subData = {
     </section>
     `,
     // 컨텐츠영역1 : new arrival
-    cont1:``,
+    cont1: `
+        <section :class="
+            'cont c1 ' +
+            $store.state.cat
+        " id="c1">
+            <!-- 2-1-1. 신상품 타이틀 -->
+            <h2 class="c1tit js-reveal">New Apporval</h2>
+            <!-- 2-1-2.신상품박스 -->
+            <div class="flowbx js-reveal">
+                <!-- 리스트박스 -->
+                <ul class="flist">
+                    <li class="m1">
+                        <a href="#">
+                            <img src="images/m1.png" alt="신상품" />
+                        </a>
+                    </li>
+                    <li class="m2">
+                        <a href="#">
+                            <img src="images/m2.png" alt="신상품" />
+                        </a>
+                    </li>
+                    <li class="m3">
+                        <a href="#">
+                            <img src="images/m3.png" alt="신상품" />
+                        </a>
+                    </li>
+                    <li class="m4">
+                        <a href="#">
+                            <img src="images/m4.png" alt="신상품" />
+                        </a>
+                    </li>
+                    <li class="m5">
+                        <a href="#">
+                            <img src="images/m5.png" alt="신상품" />
+                        </a>
+                    </li>
+                    <li class="m6">
+                        <a href="#">
+                            <img src="images/m6.png" alt="신상품" />
+                        </a>
+                    </li>
+                    <li class="m7">
+                        <a href="#">
+                            <img src="images/m7.png" alt="신상품" />
+                        </a>
+                    </li>
+                    <li class="m8">
+                        <a href="#">
+                            <img src="images/m8.png" alt="신상품" />
+                        </a>
+                    </li>
+                    <li class="m9">
+                        <a href="#">
+                            <img src="images/m9.png" alt="신상품" />
+                        </a>
+                    </li>
+                </ul>
+            </div>
+        </section>
+    `,
     // 컨텐츠영역2 : special
-    cont2:``,
+    cont2: ``,
     // 컨텐츠영역3 : 일반소개1
-    cont3:``,
+    cont3: ``,
     // 컨텐츠영역4 : 일반소개2
-    cont4:``,
-};////////////// subData ////////////////
+    cont4: ``,
+}; ////////////// subData ////////////////
 
 export default subData;
