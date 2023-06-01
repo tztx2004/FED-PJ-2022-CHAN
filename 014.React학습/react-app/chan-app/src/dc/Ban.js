efbf04bb24f2fa551e67484121ba934fce1a0247
tztx2004 <tztx2004@gmail.com>
2023-06-01 15:21:01 +0900

    230601 오후

--- 014.React학습/react-app/chan-app/src/dc/Ban.js
+++ 014.React학습/react-app/chan-app/src/dc/Ban.js
@@ -1,15 +1,43 @@
-
+// 배너 컴포넌트 - Ban.js
+import './css/ban.css'
 
 function Ban(){
+
+    const ban_data = [
+        {
+            "src":"./images/dcm21.jpg",
+            "tit1":"GOTHAM GAZETTE",
+            "tit2":"WORLDS TRAVELER",
+            "cont":`Barry who? The Flash isn't the only DC hero this summer who's been traveling through the multiverse.`,
+            "btn":"New Places, Familiar Faces",
+        },
+        {
+            "src":"./images/dcm23.jpg",
+            "tit1":"GET TO KNOW",
+            "tit2":"CASSANDRA CAIN",
+            "cont":`Whether it's as Batgirl or Orphan, Cassandra Cain stands as one of the most compelling and beloved players in the Batman mythos.`,
+            "btn":"MEET CASS",
+        },
+        {}
+    ];
+
     return(
-        <>
-            <img src="./images/dcm21.jpg" alt="배너" />
-            <section className="bantit">
-                <h3>GOTHAM GAZETTE</h3>
-                <h2>WORLDS TRAVELER</h2>
-                <p>Barry who? The Flash isn't the only DC hero this summer who's been traveling through the multiverse.</p>
-            </section>
-        </>
+        <div className="banner">
+            <ul className="slider">
+                <li>
+                    <img className='banimg' src={ban_data[0]["src"]} alt="배너" />
+                    <section className="bantit">
+                        <h3>{ban_data[0]["tit1"]}</h3>
+                        <h2>{ban_data[0]["tit2"]}</h2>
+                        <p>
+                            {ban_data[0]["cont"]}
+                        </p>
+                        <button>{ban_data[0]["btn"]}</button>
+                    </section>
+                </li>
+            </ul>
+            
+        </div>
     );
 }///// Ban ///////
 
