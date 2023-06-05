30eac0a983c53f1a6a4f94dffd202469fef72080
tztx2004 <tztx2004@gmail.com>
2023-06-05 11:15:07 +0900

    230605 오전

--- 014.React학습/react-app/chan-app/src/dc/Ban.js
+++ 014.React학습/react-app/chan-app/src/dc/Ban.js
@@ -6,56 +6,83 @@ import ban_data from './data/banner';
 // 제이쿼리
 import $ from 'jquery';
 
-$(()=>{//// jQB ////
+/* 
+    [ JS 혹은 jQuery 라우터 구현 시 로드 불일치 문제 ]
+    JS기능이 들어간 페이지의 로드구역 설정 시
+    본 페이지가 index페이지에 바로 불려질 경우
+    이 문제는 발생하지 않는다!
+    그러나 라우터로 서브 로딩 구현할 경우
+    이 구역은 라우터 모체가 로딩되는 것으로 실행된다
+    따라서 본 모듈에 적용하려고 한 의도와 달리
+    본 소스에는 적용되지 못한다! -> 라우터 로딩 불일치
 
-    // 광클금지변수
-    let prot =0;
+    [ 해결방법 : 로딩구역을 함수로 만들고 컴포넌트 소스 하단에서 호출함! ]
+    예) function jqFn(){로드구역을 포함한 소스}
+        function 컴포넌트(){
+            return(
+                <>
+                    소스들
+                    {jQFn()}
+                </>
+            )
+        }
+*/
 
-    // 1. 버튼 클릭 시 이동기능구현
-    $(".abtn").click(function(){
-        if(prot) return;
-        prot = 1;
-        setTimeout(()=>{
-            prot=0;
-        },400)
-        // 1. 버튼구분하기 
-        let isB = $(this).is(".rb");
-        console.log("오른쪽?",isB)
+// 로드구역 함수화하기! ////
+function jqFn(){
 
-        // 슬라이드 타겟설정 : 클릭된 버튼의 형제요소 슬라이더
-        const tg = $(this).siblings(".slider");
-        
-        // 2. 분기하여 기능구현하기
-        // (1) 오른쪽버튼 클릭 시 : 오른쪽에서 들어옴(left:0%-> -100%)
-        if(isB){
-            tg.animate({left:"-100%"},400,
-                function(){ // this - 타켓
-                $(this).append($(this).find("li").first())
-                .css({left:0})
-            })
+    $(()=>{//// jQB ////
+
+        // 광클금지변수
+        let prot =0;
+
+        // 1. 버튼 클릭 시 이동기능구현
+        $(".abtn").click(function(){
+            if(prot) return;
+            prot = 1;
+            setTimeout(()=>{
+                prot=0;
+            },400)
+            // 1. 버튼구분하기 
+            let isB = $(this).is(".rb");
+            console.log("오른쪽?",isB)
+
+            // 슬라이드 타겟설정 : 클릭된 버튼의 형제요소 슬라이더
+            const tg = $(this).siblings(".slider");
             
-        }
-        // (2) 왼쪽버튼 클릭 시 : 왼쪽에서 들어옴(left:-100%->0)
-        else{
-            // 마지막 li 맨앞이동+동시에 left:-100% 후 left:0 애니
-            tg.prepend(tg.find("li").last())
-            .css({left:"-100%"})
-            .animate({left:"0"},400);
-        }
+            // 2. 분기하여 기능구현하기
+            // (1) 오른쪽버튼 클릭 시 : 오른쪽에서 들어옴(left:0%-> -100%)
+            if(isB){
+                tg.animate({left:"-100%"},400,
+                    function(){ // this - 타켓
+                    $(this).append($(this).find("li").first())
+                    .css({left:0})
+                })
+                
+            }
+            // (2) 왼쪽버튼 클릭 시 : 왼쪽에서 들어옴(left:-100%->0)
+            else{
+                // 마지막 li 맨앞이동+동시에 left:-100% 후 left:0 애니
+                tg.prepend(tg.find("li").last())
+                .css({left:"-100%"})
+                .animate({left:"0"},400);
+            }
 
-        // 3. 배너와 일치하는 블릿에 클래스 "on"넣기
-        // 대상: .indic li
-        // eq(순번) -> 오른쪽이동 시 1, 왼쪽이동 시 0
-        // isB값으로 오른쪽은 true ->1, 왼쪽은 false ->0
-        // 순서가 바뀌는 슬라이드에 고유 순번속성 data-seq값을 읽어옴
-        $(".indic li").eq(tg.find("li").eq(isB).attr("data-seq")).addClass("on").siblings().removeClass("on")
-        
-    })
-    $(".indic li").click(function(){
-        $(this).addClass("on").siblings().removeClass("on")
-        $(".rb").trigger("click")
-    })
-}); /////// jQB ////////
+            // 3. 배너와 일치하는 블릿에 클래스 "on"넣기
+            // 대상: .indic li
+            // eq(순번) -> 오른쪽이동 시 1, 왼쪽이동 시 0
+            // isB값으로 오른쪽은 true ->1, 왼쪽은 false ->0
+            // 순서가 바뀌는 슬라이드에 고유 순번속성 data-seq값을 읽어옴
+            $(".indic li").eq(tg.find("li").eq(isB).attr("data-seq")).addClass("on").siblings().removeClass("on")
+            
+        })
+        $(".indic li").click(function(){
+            $(this).addClass("on").siblings().removeClass("on")
+            $(".rb").trigger("click")
+        })
+    }); /////// jQB ////////
+
+} ///////// jqFn /////////
 
 // 반복리스트 코드 생성용 컴포넌트 //////
 function MakeList(props){
@@ -107,7 +134,8 @@ function Ban(props){ // props.cat은 배너데이터 구분속성명
                     </ol>
                 </>
             }
-
+            {/* JS/jQuery 호출 */}
+            {jqFn()}
         </div>
     );
 }///// Ban ///////
