4d0c7a00fa3f3dfc4b81f5fa76e02695a45f46df
tztx2004 <tztx2004@gmail.com>
2023-06-13 14:38:24 +0900

    230613 오후

--- 700.쉐도잉프로젝트/04.DC_PJ/02.구현소스/dc-app/src/dc/Member.js
+++ 700.쉐도잉프로젝트/04.DC_PJ/02.구현소스/dc-app/src/dc/Member.js
@@ -1,7 +1,7 @@
 // Member 모듈 - Member.js
-import React,{useState} from 'react';
+import React, { useState } from "react";
 import $ from "jquery";
-import "./css/member.css"
+import "./css/member.css";
 
 /* 
     [ 후크 : Hook - 왜 필요한가? ]
@@ -28,14 +28,11 @@ import "./css/member.css"
 */
 
 // 제이쿼리 로드구역 함수
-function jqFn(){
-    $(()=>{
+function jqFn() {
+    $(() => {}); //// jQB ////
+} //////// jqFn ////////
 
-    }); //// jQB ////
-}//////// jqFn ////////
-
-function Member(){
-
+function Member() {
     // 요구사항 : 각 입력항목에 맞는 유효성검사를 입력하는 순간!
     // 실시간으로 체크하여 결과를 화면에 리턴한다!
 
@@ -44,30 +41,31 @@ function Member(){
     // 1. 아이디 변수
     const [userId, setUserId] = useState("");
     // 2. 비밀번호 변수
-    const [pwd,setPwd] = useState("");
+    const [pwd, setPwd] = useState("");
     // 3. 비밀번호 확인변수
-    const [chkpwd,setChkPwd] = useState("");
+    const [chkpwd, setChkPwd] = useState("");
     // 4. 사용자이름변수
-    const [userName,setUserName] = useState("");
+    const [userName, setUserName] = useState("");
     // 5. 이메일변수
-    const [email,setEmail] = useState("");
+    const [email, setEmail] = useState("");
 
     // [ 2. 에러상태값 후크변수 ]
     // -> 에러상태값변수 : 초기값은 에러 아님상태(false)
     // 1. 아이디에러변수
-    const [userIdError,setUserIdError] = useState(false);
+    const [userIdError, setUserIdError] = useState(false);
     // 2. 비밀번호에러변수
-    const [pwdError,setPwdError] = useState(false);
+    const [pwdError, setPwdError] = useState(false);
     // 3. 비밀번호확인에러변수
-    const [chkpwdError,setChkPwdError] = useState(false);
+    const [chkPwdError, setChkPwdError] = useState(false);
     // 4. 사용자이름에러변수
-    const [userNameError,setUserNameError] = useState(false);
+    const [userNameError, setUserNameError] = useState(false);
     // 5. 이메일에러변수
-    const [emailError,setEmailError] = useState(false);
+    const [emailError, setEmailError] = useState(false);
 
     // [ 3. 유효성 검사 메서드 ]
     // 1. 아이디 유효성 검사
-    const changeUserId = e =>{ // e - 이벤트전달변수
+    const changeUserId = (e) => {
+        // e - 이벤트전달변수
         // 1. 아이디 유효성 검사식(따옴표싸지 말것!)
         const valid = /^[A-Za-z0-9+]{5,}$/;
 
@@ -78,20 +76,17 @@ function Member(){
         // 조건: 유효성 검사결과가 true인가? 에러상태! false(에러아님)
         // 정규식.test() -> 정규식 검사결과 리턴 메서드
         // 결과: true이면 에러상태값 false / false이면 에러상태값 true
-        if(valid.test(e.target.value))
-            setUserIdError(false); // 에러아님상태!
-        else
-            setUserIdError(true); // 에러상태!
+        if (valid.test(e.target.value)) setUserIdError(false); // 에러아님상태!
+        else setUserIdError(true); // 에러상태!
 
         // 4. 실제 userId 후크변수값이 업데이트 되어야 화면에 출력됨!
         setUserId(e.target.value);
-
-    } ///// changeUserId /////
+    }; ///// changeUserId /////
 
     // 2. 비밀번호 유효성 검사
-    const changePwd = e=>{
+    const changePwd = (e) => {
         // 1. 아이디 유효성 검사식(따옴표싸지 말것!)
-        const valid = /^(?=.*[A-Za-z])(?=.*\d)[A-Za-z\d]{8,}$/
+        const valid = /^(?=.*[A-Za-z])(?=.*\d)[A-Za-z\d]{8,}$/;
 
         // 2. 입력값 확인 : e.target -> 이벤트가 발생한 요소
         console.log(e.target.value);
@@ -100,61 +95,129 @@ function Member(){
         // 조건: 유효성 검사결과가 true인가? 에러상태! false(에러아님)
         // 정규식.test() -> 정규식 검사결과 리턴 메서드
         // 결과: true이면 에러상태값 false / false이면 에러상태값 true
-        if(valid.test(e.target.value))
-            setPwdError(false); // 에러아님상태!
-        else
-            setPwdError(true); // 에러상태!
+        if (valid.test(e.target.value)) setPwdError(false); // 에러아님상태!
+        else setPwdError(true); // 에러상태!
 
         // 4. 실제 Pwd 후크변수값이 업데이트 되어야 화면에 출력됨!
         setPwd(e.target.value);
+    }; /////// changePwd ////////
 
-    }/////// changePwd ////////
-    
+    // 3. 비밀번호 확인
+    const changeChkPwd = e=>{
+        // 1. 위에 입력한 비밀번호와 일치여부
+        if(pwd === e.target.value) setChkPwdError(false)// 에러아님
+        else setChkPwdError(true)// 에러임
 
-    return(
+        // 2. 입력값 반영하기
+        setChkPwd(e.target.value);
+
+    }; ///////// changeChkPwd ///////////
+
+    // 4. 사용자이름 유효성 검사
+    const changeUserName = e=>{
+        // 1. 빈값체크
+        if(e.target.value !== "") setUserNameError(false);
+        else setUserNameError(true)
+
+        // 2. 입력값 반영하기
+        setUserName(e.target.value);
+    }
+
+    return (
         <>
             {/* 모듈코드 */}
             <section className="membx">
                 <h2>Member</h2>
                 <form>
-                {/* 1.아이디 */}
-                <label>아이디를 입력하세요</label>
-                <input type="text" maxLength="20" placeholder="아이디를 입력하세요" value={userId} onChange={changeUserId}></input>
-                {
-                    // 에러일 경우 메시지 보여주기
-                    // 조건문 && 요소 -> 조건이 true이면 요소출력
-                    userIdError && 
-                    <div className="msg">
-                        <small style={{color:"red",fontSize:"10px"}}>
-                            사용자 아이디는 5글자 이상 문자 또는 숫자를 포함해야합니다!
-                        </small>
-                    </div>
-
-                    // value={userId}값은 setUserId를 통해서만
-                    // 업데이트되어 실제화면에 반영된다!
-                    
-                    // onChange={changeUserId}
-                    // -> change이벤트 발생시 changeUserId 함수호출
-                }
-
-                {/* 2.비밀번호 */}
-                <label>비밀번호 : </label>
-                <input type="password" maxLength="20" placeholder="비밀번호를 입력하세요" value={pwd} onChange={changePwd}></input>
-
-                {
-                    pwdError && 
-                    <div className="msg">
-                        <small style={{color:"red",fontSize:"10px"}}>
-                            비밀번호는 8자 이상이어야하며 문자와 숫자를 각각 하나 이상 포함해야합니다.
-                        </small>
-                    </div>
-                }
-                {/* 3.이름 */}
-                {/* 4.이메일 */}
-                {/* 5.버튼 */}
-                {/* 6.로그인페이지링크 */}
+                    <ul>
+                        <li>
+                            {/* 1.아이디 */}
+                            <label>ID : </label>
+                            <input
+                                type="text"
+                                maxLength="20"
+                                placeholder="Please enter your ID"
+                                value={userId}
+                                onChange={changeUserId}
+                            ></input>
+                            {
+                                // 에러일 경우 메시지 보여주기
+                                // 조건문 && 요소 -> 조건이 true이면 요소출력
+                                userIdError && (
+                                    <div className="msg">
+                                        <small style={{ color: "red", fontSize: "10px" }}>
+                                            User ID must contain a minimum 5 letters
+                                        </small>
+                                    </div>
+                                )
+                                // value={userId}값은 setUserId를 통해서만
+                                // 업데이트되어 실제화면에 반영된다!
+                                // onChange={changeUserId}
+                                // -> change이벤트 발생시 changeUserId 함수호출
+                            }
+                        </li>
+                        <li>
+                            {/* 2.비밀번호 */}
+                            <label>Password : </label>
+                            <input
+                                type="password"
+                                maxLength="20"
+                                placeholder="Please enter your password"
+                                value={pwd}
+                                onChange={changePwd}
+                            ></input>
+                            {pwdError && (
+                                <div className="msg">
+                                    <small style={{ color: "red", fontSize: "10px" }}>
+                                        Password must ve at least 8 characters long and myst contain at least one letter and one capital word.
+                                    </small>
+                                </div>
+                            )}
+                        </li>
+                        <li>
+                            {/* 3.비밀번호확인 */}
+                            <label>Confirm Password : </label>
+                            <input
+                                type="password"
+                                maxLength="20"
+                                placeholder="Please enter your password"
+                                value={chkpwd}
+                                onChange={changeChkPwd}
+                            ></input>
+                            {chkPwdError && (
+                                <div className="msg">
+                                    <small style={{ color: "red", fontSize: "10px" }}>
+                                    Passwords do not match!
+                                    </small>
+                                </div>
+                            )}
+                        </li>
+                        <li></li>
+                        <li>
+                            {/* 4.이름 */}
+                            <label>name : </label>
+                            <input
+                                type="text"
+                                maxLength="20"
+                                placeholder="Please enter your name"
+                                value={userName}
+                                onChange={changeUserName}
+                            ></input>
+                            {userNameError && (
+                                <div className="msg">
+                                    <small style={{ color: "red", fontSize: "10px" }}>
+                                        This is a required input
+                                    </small>
+                                </div>
+                            )}
+                        </li>
+                        <li>{/* 5.이메일 */}</li>
+                        <li>{/* 6.버튼 */}</li>
+                        <li>{/* 7.로그인페이지링크 */}</li>
+                        <li></li>
+                    </ul>
                 </form>
-
+                
             </section>
             {/* 빈루트를 만들고 JS로드함수포함 */}
             {jqFn()}
